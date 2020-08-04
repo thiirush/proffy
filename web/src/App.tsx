a663723568eeb5017f2c36308354592b5cfdef0c
thiirush <thiiaguinbh@hotmail.com>
2020-08-03 23:26:05 -0300

    Estudar frontend implemented

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import "./assets/styles/global.css";
 
 import Landing from "../src/pages/Landing";
+import Routes from "./routes";
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <Routes />
     </div>
   );
 }
