5d11e6eb9efac776276f58112f779f1e1a3de202
thiirush <thiiaguinbh@hotmail.com>
2020-08-07 18:31:06 -0300

    Fim da NLW2, App mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+
+import { AppLoading } from "expo";
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from "@expo-google-fonts/archivo";
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
+import AppStack from "./src/routes/AppStack";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Texto alterado</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#333",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    color: "#fff",
-    fontSize: 32,
-    fontWeight: "bold",
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <AppStack />
+        <StatusBar style="light" />
+      </>
+    );
+  }
+}
---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { View, Text } from "react-native";
+import api from "../../services/api";
+import {
+  ScrollView,
+  TextInput,
+  BorderlessButton,
+  RectButton,
+} from "react-native-gesture-handler";
+
+import { Feather } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-community/async-storage";
+import PageHeader from "../components/PageHeader";
+import TeacherItem, { Teacher } from "../components/TeacherItem";
+import styles from "./styles";
+
+function TeacherList() {
+  const [teachers, setTeachers] = useState([]);
+
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
+
+  const [subject, setSubject] = useState("");
+  const [week_day, setWeekDay] = useState("");
+  const [time, setTime] = useState("");
+
+  function loadFavorites() {
+    AsyncStorage.getItem("favorites").then((response) => {
+      if (response) {
+        const favoritedTeachers = JSON.parse(response);
+
+        const favoritedTeachersIds = favoritedTeachers.map(
+          (teacher: Teacher) => {
+            return teacher.id;
+          }
+        );
+        setFavorites(favoritedTeachersIds);
+      }
+    });
+  }
+
+  function handleToggleFiltersVisisble() {
+    setIsFilterVisible(!isFilterVisible);
+  }
+
+  async function handleFiltersSubmit() {
+    loadFavorites();
+    const response = await api.get("classes", {
+      params: {
+        subject,
+        week_day,
+        time,
+      },
+    });
+    setIsFilterVisible(false);
+    setTeachers(response.data);
+  }
+
+  return (
+    <View style={styles.container}>
+      <PageHeader
+        title="Proffys disponíveis"
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersVisisble}>
+            <Feather name="filter" size={20} color="#fff" />
+          </BorderlessButton>
+        }
+      >
+        {isFilterVisible && (
+          <View style={styles.searchForm}>
+            <Text style={styles.label}>Matéria</Text>
+            <TextInput
+              value={subject}
+              onChangeText={(text) => setSubject(text)}
+              placeholderTextColor="#c1bccc"
+              style={styles.input}
+              placeholder="Qual a matéria?"
+            />
+
+            <View style={styles.inputGroup}>
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Dia da semana</Text>
+                <TextInput
+                  value={week_day}
+                  onChangeText={(text) => setWeekDay(text)}
+                  placeholderTextColor="#c1bccc"
+                  style={styles.input}
+                  placeholder="Qual o dia?"
+                />
+              </View>
+
+              <View style={styles.inputBlock}>
+                <Text style={styles.label}>Horário</Text>
+                <TextInput
+                  value={time}
+                  onChangeText={(text) => setTime(text)}
+                  placeholderTextColor="#c1bccc"
+                  style={styles.input}
+                  placeholder="Qual horário?"
+                />
+              </View>
+            </View>
+
+            <RectButton
+              onPress={handleFiltersSubmit}
+              style={styles.submitButton}
+            >
+              <Text style={styles.submitButtonText}>Filtrar</Text>
+            </RectButton>
+          </View>
+        )}
+      </PageHeader>
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{ paddingBottom: 16, paddingHorizontal: 16 }}
+      >
+        {teachers.map((teacher: Teacher) => {
+          return (
+            <TeacherItem
+              key={teacher.id}
+              teacher={teacher}
+              favorited={favorites.includes(teacher.id)}
+            />
+          );
+        })}
+      </ScrollView>
+    </View>
+  );
+}
+
+export default TeacherList;
